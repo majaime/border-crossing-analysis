63709bd66115cbadc3a5d0353216b1b2a5047c79
majaime <56524581+majaime@users.noreply.github.com>
2019-10-23 15:29:53 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Belw is an example of what might be found in this file if your program was written in Python 3.7
 #python3.7 ./src/Main.java ./input/Border_Crossing_Entry_data.csv ./output/report.csv
-javac "./src/Main.java"
+#javac ./src/Main.java
