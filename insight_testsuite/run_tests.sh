7cb6475ba44b0d810be9962f8a19439668c8b65c
majaime <56524581+majaime@users.noreply.github.com>
2019-10-23 16:09:08 -0500

    Update run_tests.sh

--- insight_testsuite/run_tests.sh
+++ insight_testsuite/run_tests.sh
@@ -2,25 +2,4 @@
 
 
 function run_all_tests {
-  TEST_FOLDERS=$(ls ${GRADER_ROOT}/tests)
-  NUM_TESTS=$(($(echo $(echo ${TEST_FOLDERS} | wc -w))))
-  PASS_CNT=0
-
-  # Loop through all tests
-  for test_folder in ${TEST_FOLDERS}; do
-
-    setup_testing_input_output
-
-    cd ${GRADER_ROOT}/temp
-    bash run.sh 2>&1
-    cd ../
-
-    compare_outputs
-  done
-
-  echo "[$(date)] ${PASS_CNT} of ${NUM_TESTS} tests passed"
-  echo "[$(date)] ${PASS_CNT} of ${NUM_TESTS} tests passed" >> ${GRADER_ROOT}/results.txt
-}
-
-check_project_struct
-run_all_tests
+  TEST_FOLDERS=$(ls ${GRADER_ROOT}/te
