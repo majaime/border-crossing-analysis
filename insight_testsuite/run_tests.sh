3286feaef59e9a4b7d352b8533a46872e2a67f97
majaime <56524581+majaime@users.noreply.github.com>
2019-10-23 15:40:19 -0500

    Update run_tests.sh

--- insight_testsuite/run_tests.sh
+++ insight_testsuite/run_tests.sh
@@ -38,9 +38,6 @@ function check_project_struct {
 # setup testing output folder
 function setup_testing_input_output {
   TEST_OUTPUT_PATH=${GRADER_ROOT}/temp
-  if [ -d ${TEST_OUTPUT_PATH} ]; then
-    rm -rf ${TEST_OUTPUT_PATH}
-  fi
 
   mkdir -p ${TEST_OUTPUT_PATH}
 
@@ -49,8 +46,7 @@ function setup_testing_input_output {
   cp -r ${PROJECT_PATH}/input ${TEST_OUTPUT_PATH}
   cp -r ${PROJECT_PATH}/output ${TEST_OUTPUT_PATH}
 
-  rm -r ${TEST_OUTPUT_PATH}/input/*
-  rm -r ${TEST_OUTPUT_PATH}/output/*
+ 
   cp -r ${GRADER_ROOT}/tests/${test_folder}/input/Border_Crossing_Entry_Data.csv ${TEST_OUTPUT_PATH}/input/Border_Crossing_Entry_Data.csv
 }
 
