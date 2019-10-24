4a313afd0b8f8dec7c94e54428d20e90224cee60
majaime <56524581+majaime@users.noreply.github.com>
2019-10-23 22:33:32 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@
 # python3.7 ./src/border_analytics.py ./input/Border_Crossing_Entry_data.csv ./output/report.csv
 cd ./src
 javac Main.java #./input/Border_Crossing_Entry_data.csv ./output/report.csv
+java Main
