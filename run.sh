9ecb46962c0da1d9a392dff78c7bdea666af01c8
majaime <56524581+majaime@users.noreply.github.com>
2019-10-24 18:48:09 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,27 @@
 # python3.7 ./src/Main.java ./input/Border_Crossing_Entry_data.csv ./output/report.csv
 #cd ./src
 #avac ./src/com/mohammadaminjavadi/Main.java #"./input/Border_Crossing_Entry_data.csv" "./output/report.csv"
-javac ./src/Main.java
-java -version
-javac -version
+#javac ./src/Main.java
+#java -version
+#javac -version
 
 #javac -cp ./src/gson-2.6.2.jar ./src/Tweet.java ./src/HashtagGraph.java ./src/Run.java
 
 #java -cp ./src/Main
+
+#!/bin/sh
+
+THE_CLASSPATH=
+PROGRAM_NAME=Main.java
+cd src
+for i in `ls ../lib/*.jar`
+    do
+    THE_CLASSPATH=${THE_CLASSPATH}:${i}
+done
+
+javac -classpath ".:${THE_CLASSPATH}" $PROGRAM_NAME
+
+if [ $? -eq 0 ]
+then
+    echo "compile worked!"
+fi
