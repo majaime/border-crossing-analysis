820392f22ba96e37f1e3e4606e20f0a5e6e7e03f
majaime <56524581+majaime@users.noreply.github.com>
2019-10-25 09:19:57 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 # python3.7 ./src/Main.java ./input/Border_Crossing_Entry_data.csv ./output/report.csv
 #cd ./src
 #avac ./src/com/mohammadaminjavadi/Main.java #"./input/Border_Crossing_Entry_data.csv" "./output/report.csv"
-javac ./src/com/mohammadaminjavadi/Main.java -d input -d output/com/mohammadaminjavadi/report.csv
+javac ./src/com/mohammadaminjavadi/Main.java -d input -cp output/com/mohammadaminjavadi/report.csv
 #java -version
 #javac -version
 
