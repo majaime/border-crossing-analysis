8aed34736953fc9fc9df79381efd58e183be1078
majaime <56524581+majaime@users.noreply.github.com>
2019-10-25 12:19:29 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
 //package com.mohammadaminjavadi;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-       System.out.println("Now the output is redirected!");
+       int a=5;
     }
 }
