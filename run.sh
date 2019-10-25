7bccecd0aa8dc776bb4a931edd8ed382b35b8df4
majaime <56524581+majaime@users.noreply.github.com>
2019-10-25 09:41:17 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -24,7 +24,8 @@
  #          classpath="xyz.jar" />
 #</target>
 
-git clone https://github.com/JetBrains/intellij-plugins
+#git clone https://github.com/JetBrains/intellij-plugins
+git clone https://github.com/JetBrains/intellij-community
 
 find ${PROJECT_DIR} -name "*.java" -print | xargs javac -g -classpath ${BUILT_PRODUCTS_DIR} -d ${BUILT_PRODUCTS_DIR}
 
